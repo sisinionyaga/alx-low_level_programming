3a473b22ce3cfd6fec886ad394128abd76ad15bd
sisinionyaga <sysynyo@gmail.com>
2023-08-15 10:58:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
 /**
-* main - A program that prints a line with puts function
+ * main - A program that prints a line with puts function
 "
-* Return: 0 (Success)
-*/
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
