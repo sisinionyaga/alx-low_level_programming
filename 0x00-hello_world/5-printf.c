38fbabd236f756e7ce883f2653c24bd0233a6225
sisinionyaga <sysynyo@gmail.com>
2023-08-15 12:10:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - Entry point
+ *
  * Return:0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
