e08120529b1cb1b88fbbe1ef8349cdeac4fcebfb
sisinionyaga <sysynyo@gmail.com>
2023-08-27 05:15:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,24 @@
 #include "main.h"
-	#include <unistd.h>
-	/**
-	 * _putchar - writes the character c to stdout
-	 * @c: The character to print
-	 *
-	 * Return: On success 1.
-	 * On error, -1 is returned, and errno is set appropriately.
-	 */
-	int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
+
+/**
+ * main - program entry point
+ *
+ * Description: prints the program _putchar
+ *
+ * Return: Always (0), meaning success
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-	void print_alphabet(void)
-	{
+/**
+ *
+ * 
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void);
+{
 		char letter;
 
 		for (letter = 'a'; letter <= 'z'; letter++)
