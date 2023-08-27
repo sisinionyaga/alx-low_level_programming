2c2c634aa4de75839af4138376eaa80d02ce59ee
sisinionyaga <sysynyo@gmail.com>
2023-08-27 05:21:58 -0700

    main file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef _MAIN_H
+#define _ MAIN_H
 
 int _putchar (char c);
 void print_alphabet(void);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
