9261ff63eb7b2e1df404016b0fb25b8e0108e872
sisinionyaga <sysynyo@gmail.com>
2023-11-25 09:14:30 -0800

    my bitwise manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
-/* the code returns unsigned interger */
+
+
+/**
+ * binary_to_uint - Entry point
+ *
+ * description: This function serves as the entry point for the program. 
+ * It tests the
+ * binary_to_uint function by calling it with different binary strings and
+ * printing the results.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+
 unsigned int binary_to_uint(const char *b) {
-    if (b == NULL)
-        return 0;
-
-    unsigned int result = 0;
-    char current;
-
-    while ((current = *b++) != '\0') {
-        if (current != '0' && current != '1') {
-            /* Invalid character encountered */
-            return 0;
-        }
-
-        result = result * 2 + (current - '0');
-    }
-
-    return result;
+	if (b == NULL)
+		return (0);
+	unsigned int result = 0;
+	char current;
+	while ((current = *b++) != '\0')
+	{
+		if
+			(current != '0' && current != '1') {
+				/* Invalid character encountered */
+				return (0);
+			}
+		result = result * 2 + (current - '0');
+	}
+	return result;
 }
-
 int main() {
-
-    const char *binaryString = "1101";
-    unsigned int result = binary_to_uint(binaryString);
-
-    if (result != 0) {
-        printf("Binary: %s\nUnsigned Integer: %u\n", binaryString, result);
-    } else {
-        printf("Invalid binary string\n");
-    }
-
-    return 0;
+	const char *binaryString = "1101";
+	unsigned int result = binary_to_uint(binaryString);
+	if (result != 0) {
+		printf("Binary: %s\nUnsigned Integer: %u\n", binaryString, result);
+	}
+	else {
+		printf("Invalid binary string\n");}
+	return (0);
 }
