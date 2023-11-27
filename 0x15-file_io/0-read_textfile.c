77f8d0a06f12976799408d0bdeb502a044709330
sisinionyaga <sysynyo@gmail.com>
2023-11-27 12:23:47 -0800

    my text files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,36 @@
-#include "main.h"
+#include "main.h
 
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
- * @letters: number of letters to print.
+ * @letters: numbers of letters printed.
  *
- * Return: number of letters printed. If it fails, returns 0.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;        /* File descriptor for the opened file */
-    ssize_t nrd, nwr;  /* Variables to store the number of bytes read and written */
-    char *buf;      /* Buffer to store the content of the file */
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-    if (!filename)
-        return (0);  /* Check for NULL filename, return 0 if invalid input */
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);  /* Open the file in read-only mode */
+	fd = open(filename, O_RDONLY);
 
-    if (fd == -1)
-        return (0);  /* Return 0 if file opening is unsuccessful */
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);  /* Allocate memory for the buffer */
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-    if (!buf)
-    {
-        close(fd);
-        return (0);  /* Return 0 if memory allocation fails */
-    }
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-    nrd = read(fd, buf, letters);  /* Read up to 'letters' bytes from the file into the buffer */
-    nwr = write(STDOUT_FILENO, buf, nrd);  /* Write the content of the buffer to standard output */
+	close(fd);
 
-    close(fd);  /* Close the file descriptor */
-    free(buf);  /* Free the allocated memory for the buffer */
+	free(buf);
 
-    return (nwr);  /* Return the number of bytes written to standard output */
+	return (nwr);
 }
