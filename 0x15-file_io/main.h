11652339b2c1b3a4f64f9c20657178709cfb48f4
sisinionyaga <sysynyo@gmail.com>
2023-12-18 07:13:46 -0800

    my prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
-#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-/* 0. Tread lightly, she is near */
 ssize_t read_textfile(const char *filename, size_t letters);
-
-/* 1. Under the snow */
 int create_file(const char *filename, char *text_content);
-
-/* 2. Speak gently, she can hear */
 int append_text_to_file(const char *filename, char *text_content);
 
-/* 3. cp */
-int copy_file(const char *file_from, const char *file_to);
-
-/* 4. elf */
-void display_elf_header(const char *elf_filename);
-
-#endif /* MAIN_H */
+#endif
